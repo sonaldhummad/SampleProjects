13567190e4d19e2ac558fc30a60b1aedb9d077bc
sandip <sandipsinh.dhummad@spglobal.com>
2018-05-27 09:31:33 -0400

    employee class added

---
+++ SpringRest/src/main/java/com/springBoot/SpringRest/Employee.java
@@ -0,0 +1,46 @@
+package com.springBoot.SpringRest;
+
+public class Employee {
+
+	 public String empId;
+	    public String firstName;
+	    public String lastName;
+		/**
+		 * @return the empId
+		 */
+		public String getEmpId() {
+			return empId;
+		}
+		/**
+		 * @param empId the empId to set
+		 */
+		public void setEmpId(String empId) {
+			this.empId = empId;
+		}
+		/**
+		 * @return the firstName
+		 */
+		public String getFirstName() {
+			return firstName;
+		}
+		/**
+		 * @param firstName the firstName to set
+		 */
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		/**
+		 * @return the lastName
+		 */
+		public String getLastName() {
+			return lastName;
+		}
+		/**
+		 * @param lastName the lastName to set
+		 */
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+	    
+	    
+}
